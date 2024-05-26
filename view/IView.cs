b13290a5a3e6b7b72451d9373ffd9d817cda7a67
Aergm <aergm145@mail.ru>
2024-05-26 16:38:13 +0300

    Описаны все необходимые интерфейсы view

---
+++ view/IView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using WinFormApp1.library;
+
+namespace WinFormApp1.view
+{
+    interface IView
+    {
+        void displayCategory(string categoryName);
+        void displayProduct();
+        void addInCart();
+        void removeFromCart();
+        void clearCart();
+        void showErrorPaymentMessage();
+        void showSuccessPaymentMessage();
+        void fillProductList();
+        void fillCategories();
+    }
+}
