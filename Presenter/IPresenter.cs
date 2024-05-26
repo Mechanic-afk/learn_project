974d6642ccf7bb3ff23ecdfaf367be738fe64676
Aergm <aergm145@mail.ru>
2024-05-26 15:47:21 +0300

    Закончил список необходимых интерфейсов

---
+++ Presenter/IPresenter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinFormApp1.library;
+
+namespace WinFormApp1.presenter
+{
+    interface IPresenter
+    {
+        Product getProductByName(string name);
+        List<Product> getProductsByCategory(string selectedCategory);
+        List<string> getCategories();
+        List<string> getPaymentVariants();
+
+        int sumPrices(ListBox listBox);
+        int getCardAmount();
+        int getCashAmount();
+        bool Pay(int moneyAmount, string paymentVariant);
+
+        void startNewSession();
+    }
+}
